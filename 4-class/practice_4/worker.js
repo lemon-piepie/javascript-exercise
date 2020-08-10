fa1242387125c9e4acadde21ac83e3e43d43f46e
lemon-piepie <purplechilde@163.com>
2020-08-10 18:34:47 +0800

    finish exercise four

--- 4-class/practice_4/worker.js
+++ 4-class/practice_4/worker.js
@@ -1,1 +1,12 @@
 // TODO 17: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  introduce() {
+    return `${super.basic_introduce()} I am a Worker. I have a job.`;
+  }
+}
