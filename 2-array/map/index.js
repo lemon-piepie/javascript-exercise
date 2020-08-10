c6b55c93737a9da109310cec3c40f1c06f1dee00
lemon-piepie <purplechilde@163.com>
2020-08-10 16:30:05 +0800

    finish array all test

--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,7 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  function transform(num) {
+    return String.fromCharCode(num + 96);
+  }
+  return collection.map(transform);
 }
