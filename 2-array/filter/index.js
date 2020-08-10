78d15e3a6cc93f2fa43171d6e30af4b13c35cde0
lemon-piepie <purplechilde@163.com>
2020-08-10 16:07:29 +0800

    finish array second test

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,18 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  const result = collection.filter(i => i % 3 === 0);
+  return result;
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  const result = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (!result.includes(collection[i])) {
+      result.push(collection[i]);
+    }
+  }
+  return result;
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,5 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  const result = collection.find(item => item.age <= 20 && item.age >= 10);
+  return result.name;
 }
