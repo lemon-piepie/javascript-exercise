b0c7a201ec81221b5c76df39174e8b787593b45f
lemon-piepie <purplechilde@163.com>
2020-08-10 15:20:56 +0800

    finish string first test

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,10 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  const result = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i].substr(0, 2) === '粤A') {
+      result.push(collection[i]);
+    }
+  }
+  return result.length;
 }
