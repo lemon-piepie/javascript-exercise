a4c52db3a3e081925d49f1c807a6b69a263aee61
lemon-piepie <purplechilde@163.com>
2020-08-10 17:48:05 +0800

    finish obejcet all test

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,9 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  let sum = 0;
+  const values = Object.values(source);
+  for (let i = 0; i < values.length; i++) {
+    sum += parseInt(values[i]);
+  }
+  return sum;
 }
